560e410b1dc99cd325452c035b6c6e42691f0e40
Sriram Chakka <srchakka@cisco.com>
2021-02-02 17:14:50 +0530

    Adding unit test as requested

---
+++ src/genie/libs/parser/iosxr/tests/ShowPlatform/cli/equal/golden_output6_expected.py
@@ -0,0 +1,53 @@
+expected_output = {
+    "slot": {
+        "lc": {
+            "0/2": {
+                "name": "NCS1K4-OTN-XP",
+                "full_slot": "0/2",
+                "state": "POWERED_ON",
+                "config_state": "NSHUT",
+            },
+        },
+        "oc": {
+            "0/FT0": {
+                "name": "NCS1K4-FAN",
+                "full_slot": "0/FT0",
+                "state": "OPERATIONAL",
+                "config_state": "NSHUT",
+            },
+            "0/FT1": {
+                "name": "NCS1K4-FAN",
+                "full_slot": "0/FT1",
+                "state": "OPERATIONAL",
+                "config_state": "NSHUT",
+            },
+            "0/FT2": {
+                "name": "NCS1K4-FAN",
+                "full_slot": "0/FT2",
+                "state": "OPERATIONAL",
+                "config_state": "NSHUT",
+            },
+            "0/PM0": {
+                "name": "NCS1K4-AC-PSU",
+                "full_slot": "0/PM0",
+                "state": "OPERATIONAL",
+                "config_state": "NSHUT",
+            },
+            "0/SC0": {
+                "name": "NCS1004",
+                "full_slot": "0/SC0",
+                "state": "OPERATIONAL",
+                "config_state": "NSHUT",
+            },
+        },
+        "rp": {
+            "0/RP0": {
+                "name": "NCS1K4-CNTLR-K9",
+                "full_slot": "0/RP0/CPU0",
+                "state": "IOS XR RUN",
+                "config_state": "NSHUT",
+                "redundancy_state": "Active",
+            },
+        }
+    }
+}
